c33557551a79ea6f0bd6708e1001c81edf9b0a52
jopaa10 <jopamate@gmail.com>
2020-12-12 23:07:27 +0100

    adding Create awesome photos component for homepage

---
+++ src/components/create - awesomePhotos/index.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import {graphql, useStaticQuery} from 'gatsby'
+
+//bootstrap
+
+import {Col, Row, Container, Card} from 'react-bootstrap'
+
+//gatsby image
+
+import BackgroundImage from 'gatsby-background-image'
+
+//fontawesome
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faCamera, faEye, faHourglassHalf} from '@fortawesome/free-solid-svg-icons'
+
+
+const CreateAwesomePhotos = () =>{
+
+    const data = useStaticQuery(graphql`
+        query{
+    
+            bgImage: file(relativePath: {eq: "wallpaperKids.jpg"}){
+                childImageSharp{
+                    fluid(maxWidth: 4000, quality: 100){
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+        }
+    
+    `)
+
+
+    return(
+        <BackgroundImage fluid={data.bgImage.childImageSharp.fluid} className="banner bannerBg" style={{backgroundPositionY: '35%', backgroundAttachment: 'initial'}}>
+            <section >
+                
+                <Container>
+                    <Row>
+                        <Col xs={12} style={{textAlign: 'center'}}>
+                            <h3 style={{fontFamily: "josefin sans", fontSize: "2.5em", lineHeight: "1.5", color: 'white'}}>Create Awesome Pro Photos</h3>
+                        </Col>
+                    </Row>
+                    <Row style={{textAlign: '-webkit-center'}}>
+                        <Col md={12} lg={4} style={{paddingBottom: '10px'}}>
+                            <Card style={{ width: '14rem', boxShadow: '0 0 45px rgba(0,0,0,.1)' }}>
+                            <Card.Body>
+                                <FontAwesomeIcon style={{width: '45px', height: '45px'}} icon={faCamera}/>
+                                <Card.Title>Camera</Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                                </Card.Text>
+                            
+                            </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col md={12} lg={4} style={{paddingBottom: '10px'}}>
+                            <Card style={{ width: '14rem', boxShadow: '0 0 45px rgba(0,0,0,.1)' }}>
+                            <Card.Body>
+                                <FontAwesomeIcon style={{width: '45px', height: '45px'}} icon={faEye}/>
+                                <Card.Title>Eye for details</Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                                </Card.Text>
+                                
+                            </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col md={12} lg={4} style={{paddingBottom: '10px'}}>
+                            <Card style={{ width: '14rem', boxShadow: '0 0 45px rgba(0,0,0,.1)' }}>
+                                <Card.Body>
+                                    <FontAwesomeIcon style={{width: '45px', height: '45px'}} icon={faHourglassHalf}/>
+                                    <Card.Title>Time for practice</Card.Title>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
+                                    </Card.Text>
+                                    
+                                </Card.Body>
+                                </Card>
+                            </Col>
+                        </Row>
+                    
+                    </Container>  
+
+                    <Container className="banner" style={{backgroundColor: 'transparent', paddingTop: '0px'}}>
+
+                        
+                    </Container>
+
+             </section>
+
+     </BackgroundImage>
+    
+
+    )
+}
+
+export default CreateAwesomePhotos
--- src/components/homepage/layout.js
+++ src/components/homepage/layout.js
@@ -14,6 +14,7 @@ import "../layout.css"
 //pages
 import Banner from '../banner/index'
 import PhotoInfo from '../carousel - homepage/index'
+import CreateAwesomePhotos from "../create - awesomePhotos"
 
 const Layout = () => {
 
@@ -33,13 +34,14 @@ const Layout = () => {
     <>
       <Header />
       <main>
-      <Banner
-            color={'white'} 
-            title={'"Photography is the story I fail to put into words"'}
-            author={'Denise Sparks'}
-            bgImage={data.bgImage.childImageSharp.fluid}
-          />
-      <PhotoInfo />
+        <Banner
+              color={'white'} 
+              title={'"Photography is the story I fail to put into words"'}
+              author={'Denise Sparks'}
+              bgImage={data.bgImage.childImageSharp.fluid}
+        />
+        <PhotoInfo />
+        <CreateAwesomePhotos />
       </main>
     </>
   )
